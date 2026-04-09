ece3b98c1ebb0998678856064d97f405ac6c8942
bearmarketroaster <richardstorkey@bearmarket.ie>
2026-04-09 09:45:36 +0100

    Uploaded config file

---
+++ config-js-ref.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  SUPABASE_URL:      "https://rcykffqwcjitsbyiilzw.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjeWtmZnF3Y2ppdHNieWlpbHp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MzYwMzksImV4cCI6MjA5MTIxMjAzOX0.dwHlOpA9itRbphEslNhmiMFiznIo0S-pV1wzkJlnhZ8",
+  APP_NAME:    "Bear Roastery",
+  APP_VERSION: "0.1.0",
+};
